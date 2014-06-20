8a8206faa9c03c1c4ce0b4cde76dc0f11216a749
frankibem <soulsolja@yahoo.com>
2014-06-20 17:26:18 -0500

    df

---
+++ Test Soccer/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyForce : MonoBehaviour
+{
+
+    public float timeToShoot = 2f;
+    private float timer = 0f;
+}
---
+++ Test Soccer/Assets/Scripts/Coords.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coords : MonoBehaviour
+{
+    public Transform ball;
+    public GUIText x;
+    public GUIText y;
+    public string xCod;
+    public string yCod;
+
+    private bool canGo = false;
+
+    void Update()
+    {
+        if (canGo)
+        {
+            ball.position = Vector2.Lerp(ball.position, new Vector3(float.Parse(xCod), float.Parse(yCod), ball.position.z), 5 * Time.deltaTime);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 220, 130), "Displacement");
+        xCod = GUI.TextField(new Rect(15, 25, 200, 20), xCod, 25);
+        yCod = GUI.TextField(new Rect(15, 55, 200, 20), yCod, 25);
+
+        if (GUI.Button(new Rect(20, 75, 80, 20), "Displace"))
+        {
+            canGo = true;
+        }
+    }
+}
